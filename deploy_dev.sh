4028141b2cc2dbeac980d94a9971aa8166730ae6
Lutfunnahar <l.nahar@pipelinesecurity.net>
2022-03-28 10:10:42 +0600

    updated file

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -5,5 +5,8 @@ pwd
 cp -a dist/flaskr-1.0.0-py3-none-any.whl /var/lib/jenkins/workspace/test-deploy/
 cd /var/lib/jenkins/workspace/test-deploy
 python3.8 -m venv venv
+. venv/bin/activate
+pip3.8 install flaskr-1.0.0-py3-none-any.whl
+export FLASK_APP=flaskr
 echo "Connecting deployment server successfully"
 echo "Complete deploy"
